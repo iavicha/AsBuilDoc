f139ab29b081e92e768e2f5bec4e1fffc3be4c1b
iliavetchinin <ivetchinin@gmail.com>
2021-03-06 03:17:29 +0300

    fix: test

--- database_app/tests.py
+++ database_app/tests.py
@@ -8,5 +8,5 @@ class JobTest(TestCase):
         Job.objects.create(name='Работа', start_date="2021-02-21", end_date="2021-02-22")
 
     def test_job_name(self):
-        job_name = Job.objects.get(start_date="22-02-2021")
-        self.assertEqual(str(job_name.name()), 'Работа')
+        job_name = Job.objects.get(name='Работа')
+        self.assertEqual(str(job_name.start_date), "2021-02-21")
